3ba456a8e78402377b2597e6ebfb9f54e1fd30be
AS3-1-03 <AS3-1-03@uni.lan>
2019-04-09 12:36:42 +0200

    a

---
+++ servidores/ejemplo_web/webapp2.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World2!');
+});
+
+app.get('/info', function (req, res) {
+  res.send('The info of ...');
+});
+
+app.get('/web',function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
